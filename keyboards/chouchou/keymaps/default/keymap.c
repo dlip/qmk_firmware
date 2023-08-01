9573a82d13a90d5fd4b5a3c661c2c30b5243bd35
Dane Lipscombe <danelipscombe@gmail.com>
2023-08-01 20:41:59 +1000

    Add taipo keycode enum

--- keyboards/chouchou/keymaps/default/keymap.c
+++ keyboards/chouchou/keymaps/default/keymap.c
@@ -13,19 +13,19 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_TAIPO] = LAYOUT_split_2x4_2(
-        0,            1,            2,            3,            13,         12,           11,           10,
-        4,            5,            6,            7,            17,         16,           15,           14,
-                                    8,            9,            19,         18
+       TP_TLP,        TP_TLR,    TP_TLM,   TP_TLI,          TP_TRI,     TP_TRM,     TP_TRR,     TP_TRP,
+       TP_BLP,        TP_BLR,    TP_BLM,   TP_BLI,          TP_BRI,     TP_BRM,     TP_BRR,     TP_BRP,
+                                 TP_LIT,   TP_LOT,          TP_ROT,     TP_RIT
     ),
     [_GAMING] = LAYOUT_split_2x4_2(
         KC_TAB,       KC_Q,      KC_W,     KC_E,            KC_U,       KC_UP,       KC_O,      KC_ESC,
         KC_LSFT,      KC_A,      KC_S,     KC_D,            KC_LEFT,    KC_DOWN,     KC_RIGHT,  KC_SCLN,
-                              KC_LCTL,   KC_SPC,            KC_ENTER,   TO(_TAIPO)
+                                 KC_LCTL,  KC_SPC,          KC_ENTER,   TO(_TAIPO)
     ),
     [_GAMEPAD] = LAYOUT_split_2x4_2(
-        GP_HOM,      GP_LSB,    GP_DPU,    GP_RSB,          GP_X,     GP_Y,     GP_RB,     GP_LB,
-        GP_BCK,      GP_DPL,    GP_DPD,    GP_DPR,          GP_A,     GP_B,     GP_RT,     GP_LT,
-                                XXXXXXX,   GP_DPU,          GP_STA,   TO(_TAIPO)
+        GP_HOM,      GP_LSB,    GP_DPU,    GP_RSB,          GP_X,       GP_Y,        GP_RB,     GP_LB,
+        GP_BCK,      GP_DPL,    GP_DPD,    GP_DPR,          GP_A,       GP_B,        GP_RT,     GP_LT,
+                                XXXXXXX,   GP_DPU,          GP_STA,     TO(_TAIPO)
     )
 };
 
