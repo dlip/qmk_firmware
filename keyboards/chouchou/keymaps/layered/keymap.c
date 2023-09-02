d6ed3417eb04b03a5628fced63e7655bd07ac020
Dane Lipscombe <danelipscombe@gmail.com>
2023-09-02 16:11:55 +1000

    add conditional layer locking

--- keyboards/chouchou/keymaps/layered/keymap.c
+++ keyboards/chouchou/keymaps/layered/keymap.c
@@ -2,27 +2,32 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
 enum custom_keycodes {
-    CB_LFT = QK_USER, 
-    CB_RGT
+    CK_LFT = QK_USER, 
+    CK_RGT
 };
 
 
 enum my_layers {
     _BASE,
-    _ALPHA,
+    _LFT,
+    _RGT,
     _NAV,
-    _LEFT
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_2x4_2(
         KC_ESC,       KC_L,      KC_D,         KC_Y,            KC_M,       KC_O,        KC_U,      KC_BSPC,
         KC_N,         KC_R,      KC_T,         KC_S,            KC_H,       KC_A,        KC_I,      KC_E,
-                                 CB_LFT,       KC_SPC,          KC_LSFT,    CB_RGT
+                                 CK_LFT,       KC_SPC,          KC_LSFT,    CK_RGT
     ),
-    [_ALPHA] = LAYOUT_split_2x4_2(
+    [_LFT] = LAYOUT_split_2x4_2(
         KC_DEL,       KC_X,      KC_V,         KC_G,            KC_K,       KC_Q,        KC_COMMA,  KC_ENTER,
         KC_P,         KC_W,      KC_B,         KC_C,            KC_F,       KC_Z,        KC_DOT,    KC_J,
+                                 KC_TRNS,      KC_TRNS,         KC_TRNS,    KC_TRNS
+    ),
+    [_RGT] = LAYOUT_split_2x4_2(
+        KC_INS,       KC_ESC,    KC_UP,        KC_ENTER,        KC_K,       KC_Q,        KC_COMMA,  KC_ENTER,
+        KC_DEL,       KC_LEFT,   KC_DOWN,      KC_RIGHT,        KC_F,       KC_Z,        KC_DOT,    KC_J,
                                  KC_TRNS,      KC_TRNS,         KC_TRNS,    KC_TRNS
     ),
     [_NAV] = LAYOUT_split_2x4_2(
@@ -30,26 +35,46 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_DEL,       KC_LEFT,   KC_DOWN,      KC_RIGHT,        KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
                                  KC_TRNS,      KC_LSFT,         KC_TRNS,    KC_TRNS
     ),
-    [_LEFT] = LAYOUT_split_2x4_2(
-        KC_PSCR,      KC_ESC,    C(KC_Z),      KC_ENTER,        KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
-        KC_DEL,       C(KC_X),   C(KC_C),      C(KC_V),         KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
-                                 KC_TRNS,      KC_LSFT,         KC_TRNS,    KC_TRNS
-    ),
+    // [_LFT] = LAYOUT_split_2x4_2(
+    //     KC_PSCR,      KC_ESC,    C(KC_Z),      KC_ENTER,        KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+    //     KC_DEL,       C(KC_X),   C(KC_C),      C(KC_V),         KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+    //                              KC_TRNS,      KC_LSFT,         KC_TRNS,    KC_TRNS
+    // ),
 };
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case CB_RGT:
+    case CK_RGT:
         if (record->event.pressed) {
-           set_oneshot_layer(_ALPHA, ONESHOT_START);
+            if (IS_LAYER_ON(_RGT)) {
+                layer_move(_BASE);
+            } else {
+                layer_move(_RGT);
+            }
         }
         return false;
+    case CK_LFT:
+            if (IS_LAYER_ON(_LFT)) {
+                layer_move(_BASE);
+            } else {
+                layer_move(_LFT);
+            }
+        return false;
     default:
-        if (IS_LAYER_ON(_ALPHA) && keycode != KC_LSFT) {
-            tap_code16(keycode);
-            clear_oneshot_layer_state(_ALPHA);
-            return false;
+        if (record->event.pressed && keycode >= KC_A && keycode <= KC_Z) {
+            if (IS_LAYER_ON(_RGT) && keycode != KC_LSFT) {
+                tap_code16(keycode);
+                layer_move(_BASE);
+                // clear_oneshot_layer_state(_RGT);
+                return false;
+            }
+            if (IS_LAYER_ON(_LFT) && keycode != KC_LSFT) {
+                tap_code16(keycode);
+                layer_move(_BASE);
+                // clear_oneshot_layer_state(_LFT);
+                return false;
+            }
         }
         return true;
   }
