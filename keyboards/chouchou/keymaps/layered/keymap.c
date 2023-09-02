9a9e87f8c924d9395443df218425d5e3d6bcb5f4
Dane Lipscombe <danelipscombe@gmail.com>
2023-09-02 16:59:33 +1000

    colmak layered

--- keyboards/chouchou/keymaps/layered/keymap.c
+++ keyboards/chouchou/keymaps/layered/keymap.c
@@ -16,18 +16,18 @@ enum my_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_2x4_2(
-        KC_ESC,       KC_L,      KC_D,         KC_Y,            KC_M,       KC_O,        KC_U,      KC_BSPC,
-        KC_N,         KC_R,      KC_T,         KC_S,            KC_H,       KC_A,        KC_I,      KC_E,
+        KC_Q,         KC_W,      KC_F,         KC_Y,            KC_L,       KC_U,        KC_Y,      KC_BSPC,
+        KC_A,         KC_R,      KC_S,         KC_T,            KC_N,       KC_E,        KC_I,      KC_O,
                                  CK_LFT,       KC_SPC,          KC_LSFT,    CK_RGT
     ),
     [_LFT] = LAYOUT_split_2x4_2(
-        KC_DEL,       KC_X,      KC_V,         KC_G,            KC_K,       KC_Q,        KC_COMMA,  KC_ENTER,
-        KC_P,         KC_W,      KC_B,         KC_C,            KC_F,       KC_Z,        KC_DOT,    KC_J,
-                                 KC_TRNS,      KC_TRNS,         KC_TRNS,    KC_TRNS
+        KC_NO,        KC_B,      KC_G,         KC_V,            KC_5,       KC_6,        KC_7,      KC_8,
+        KC_Z,         KC_X,      KC_C,         KC_D,            KC_1,       KC_2,        KC_3,      KC_4,
+                                 KC_TRNS,      KC_LSFT,         KC_0,       KC_9
     ),
     [_RGT] = LAYOUT_split_2x4_2(
-        KC_INS,       KC_ESC,    KC_UP,        KC_ENTER,        KC_K,       KC_Q,        KC_COMMA,  KC_ENTER,
-        KC_DEL,       KC_LEFT,   KC_DOWN,      KC_RIGHT,        KC_F,       KC_Z,        KC_DOT,    KC_J,
+        KC_INS,       KC_ESC,    KC_UP,        KC_ENTER,        KC_K,       KC_M,        KC_J,      KC_SCLN,
+        KC_DEL,       KC_LEFT,   KC_DOWN,      KC_RIGHT,        KC_H,       KC_COMM,     KC_DOT,    KC_QUOT,
                                  KC_TRNS,      KC_TRNS,         KC_TRNS,    KC_TRNS
     ),
     [_NAV] = LAYOUT_split_2x4_2(
@@ -55,24 +55,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
         return false;
     case CK_LFT:
+        if (record->event.pressed) {
             if (IS_LAYER_ON(_LFT)) {
                 layer_move(_BASE);
             } else {
                 layer_move(_LFT);
             }
+        }
         return false;
     default:
-        if (record->event.pressed && keycode >= KC_A && keycode <= KC_Z) {
+        if (record->event.pressed) {
             if (IS_LAYER_ON(_RGT) && keycode != KC_LSFT) {
                 tap_code16(keycode);
                 layer_move(_BASE);
-                // clear_oneshot_layer_state(_RGT);
                 return false;
             }
             if (IS_LAYER_ON(_LFT) && keycode != KC_LSFT) {
                 tap_code16(keycode);
                 layer_move(_BASE);
-                // clear_oneshot_layer_state(_LFT);
                 return false;
             }
         }
