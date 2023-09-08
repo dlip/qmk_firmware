c6b84f43c5bd1539b8247a513e92e1439a70e4c4
Dane Lipscombe <danelipscombe@gmail.com>
2023-09-08 13:59:30 +1000

    switch to canary layout

--- keyboards/mushi/keymaps/dlip/keymap.c
+++ keyboards/mushi/keymaps/dlip/keymap.c
@@ -10,14 +10,14 @@ enum mylayers {
     _MED,
 };
 
-#define KC_MED_A LT(_MED, KC_A)
+#define KC_MED_C LT(_MED, KC_C)
 #define KC_ALT_R MT(MOD_LALT, KC_R)
 #define KC_GUI_S MT(MOD_LGUI, KC_S)
 #define KC_CTL_T MT(MOD_LCTL, KC_T)
 #define KC_CTL_N MT(MOD_LCTL, KC_N)
 #define KC_GUI_E MT(MOD_LGUI, KC_E)
 #define KC_ALT_I MT(MOD_LALT, KC_I)
-#define KC_MED_O LT(_MED, KC_O)
+#define KC_MED_A LT(_MED, KC_A)
 #define KC_NAV MO(_NAV)
 #define KC_NUM MO(_NUM)
 #define KC_OS_SFT OSM(KC_LSFT)
@@ -32,9 +32,9 @@ enum mylayers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BSE] = LAYOUT_split_3x5_3(
-         KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,             KC_J,      KC_L,     KC_U,     KC_Y,     KC_SCLN,
-         KC_MED_A, KC_ALT_R, KC_GUI_S, KC_CTL_T, KC_G,             KC_M,      KC_CTL_N, KC_GUI_E, KC_ALT_I, KC_MED_O,
-         KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,             KC_K,      KC_H,     KC_COMMA, KC_DOT,   KC_QUOT,
+         KC_W,     KC_L,     KC_Y,     KC_P,     KC_B,             KC_Z,      KC_F,     KC_O,     KC_U,     KC_QUOT,
+         KC_MED_C, KC_ALT_R, KC_GUI_S, KC_CTL_T, KC_G,             KC_M,      KC_CTL_N, KC_GUI_E, KC_ALT_I, KC_MED_A,
+         KC_Q,     KC_J,     KC_V,     KC_D,     KC_K,             KC_X,      KC_H,     KC_SCLN,  KC_COMMA, KC_DOT,
                              KC_NUM,   KC_SPC,   KC_OS_SFT,        KC_OS_SFT, KC_BSPC,  KC_NAV
     ),
     [_NUM] = LAYOUT_split_3x5_3(
