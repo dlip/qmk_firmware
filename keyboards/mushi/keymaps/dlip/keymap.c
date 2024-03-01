b7378c3562a624cc1a7713f8dc1d0d2b0e04b71a
Dane Lipscombe <danelipscombe@gmail.com>
2024-03-01 12:18:44 +1100

    Update abbrs

--- keyboards/mushi/keymaps/dlip/keymap.c
+++ keyboards/mushi/keymaps/dlip/keymap.c
@@ -19,7 +19,6 @@ enum mylayers {
 #define KC_ALT_R MT(MOD_LALT, KC_R)
 #define KC_GUI_S MT(MOD_LGUI, KC_S)
 #define KC_CTL_T MT(MOD_LCTL, KC_T)
-#define KC_SFT_D MT(MOD_LSFT, KC_D)
 #define KC_CAG_V LCAG_T(KC_V)
 #define KC_CAG_SCLN LCAG_T(KC_SCLN)
 #define KC_SFT_H MT(MOD_LSFT, KC_H)
@@ -59,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BSE] = LAYOUT_split_3x5_3(
          KC_W,     KC_L,     KC_Y,     KC_P,       KC_B,             KC_Z,        KC_F,       KC_O,        KC_U,     KC_QUOT,
          KC_SFT_C, KC_ALT_R, KC_GUI_S, KC_CTL_T,   KC_G,             KC_M,        KC_CTL_N,   KC_GUI_E,    KC_ALT_I, KC_SFT_A,
-         KC_Q,     KC_J,     KC_CAG_V, KC_SFT_D,   KC_K,             KC_X,        KC_H,       KC_CAG_SCLN, KC_COMMA, KC_DOT,
+         KC_Q,     KC_J,     KC_CAG_V, KC_D,       KC_K,             KC_X,        KC_H,       KC_CAG_SCLN, KC_COMMA, KC_DOT,
                              KC_MED,   KC_NNM_TAB, KC_MED_SPC,       KC_SFT_BSPC, KC_COMBO,   QK_REPEAT_KEY
     ),
     [_NNM] = LAYOUT_split_3x5_3(
