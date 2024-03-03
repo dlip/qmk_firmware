8cbd0fe35abd35506b283b8fcde4854c686cacd6
Dane Lipscombe <danelipscombe@gmail.com>
2024-03-03 14:49:09 +1100

    Fix layout

--- keyboards/mushi/keymaps/dlip/keymap.c
+++ keyboards/mushi/keymaps/dlip/keymap.c
@@ -12,7 +12,6 @@ enum mylayers {
     _BSE,
     _NNM,
     _MED,
-    _TPO
 };
 
 #define KC_SFT_C MT(MOD_LSFT, KC_C)
@@ -36,13 +35,20 @@ enum mylayers {
 #define KC_CAG_PGDN LCAG_T(KC_PGDN)
 
 #define KC_CAG_2 LCAG_T(KC_2)
-#define KC_ALT_6 MT(MOD_LALT, KC_6)
-#define KC_GUI_5 MT(MOD_LGUI, KC_5)
 #define KC_CTL_4 MT(MOD_LCTL, KC_4)
+#define KC_GUI_5 MT(MOD_LGUI, KC_5)
+#define KC_ALT_6 MT(MOD_LALT, KC_6)
+#define KC_SFT_0 MT(MOD_LSFT, KC_0)
 
-#define KC_ALT_MPRV MT(MOD_LALT, KC_MPRV)
-#define KC_GUI_MPLY MT(MOD_LGUI, KC_MPLY)
-#define KC_CTL_MNXT MT(MOD_LCTL, KC_MNXT)
+#define KC_SFT_UDO MT(MOD_LSFT, C(KC_Z))
+#define KC_ALT_CUT MT(MOD_LALT, C(KC_X))
+#define KC_GUI_CPY MT(MOD_LGUI, C(KC_C))
+#define KC_CTL_PST MT(MOD_LCTL, C(KC_V))
+
+#define KC_CTL_F4 MT(MOD_LCTL, KC_F4)
+#define KC_GUI_F5 MT(MOD_LGUI, KC_F5)
+#define KC_ALT_F6 MT(MOD_LALT, KC_F6)
+#define KC_SFT_F10 MT(MOD_LSFT, KC_F10)
 
 #define KC_SFT_BSPC MT(MOD_LSFT, KC_BSPC)
 #define KC_NNM_TAB LT(_NNM, KC_TAB)
@@ -62,16 +68,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_MED,   KC_NNM_TAB, KC_MED_SPC,       KC_SFT_BSPC, KC_COMBO,   QK_REPEAT_KEY
     ),
     [_NNM] = LAYOUT_split_3x5_3(
-         KC_GRV,     KC_ESC,     KC_UP,       KC_ENT,     KC_DEL,         KC_BSLS,  KC_7,     KC_8,     KC_9,     KC_SLSH,
-         KC_TAB,     KC_ALT_LFT, KC_GUI_DWN,  KC_CTL_RGT, KC_LBRC,        KC_EQL,   KC_CTL_4, KC_GUI_5, KC_ALT_6, KC_0,
-         KC_HOME,    KC_PGUP,    KC_CAG_PGDN, KC_SFT_END, KC_RBRC,        KC_MINUS, KC_1,     KC_CAG_2, KC_3,     KC_DOT,
-                                 KC_TRNS,     KC_TRNS,    KC_TRNS,        KC_TRNS,  KC_TRNS,  KC_TRNS
+         KC_GRV,      KC_ESC,     KC_UP,       KC_ENT,     KC_DEL,         KC_BSLS,  KC_7,     KC_8,     KC_9,     KC_SLSH,
+         KC_SFT_BSPC, KC_ALT_LFT, KC_GUI_DWN,  KC_CTL_RGT, KC_LBRC,        KC_EQL,   KC_CTL_4, KC_GUI_5, KC_ALT_6, KC_SFT_0,
+         KC_HOME,     KC_PGUP,    KC_CAG_PGDN, KC_SFT_END, KC_RBRC,        KC_MINUS, KC_1,     KC_CAG_2, KC_3,     KC_DOT,
+                                  KC_TRNS,     KC_TRNS,    KC_TRNS,        KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
     [_MED] = LAYOUT_split_3x5_3(
-         G(KC_Z), G(KC_X),     G(KC_C),     G(KC_V),     KC_VOLU,        TO(_TPO), KC_F7,   KC_F8, KC_F9, KC_F11,
-         KC_PSCR, KC_ALT_MPRV, KC_GUI_MPLY, KC_CTL_MNXT, KC_VOLD,        QK_BOOT,  KC_F4,   KC_F5, KC_F6, KC_F10,
-         C(KC_Z), C(KC_X),     C(KC_C),     C(KC_V),     KC_PSCR,        KC_NO,    KC_F1,   KC_F2, KC_F3, KC_F12,
-                               KC_TRNS,     KC_TRNS,     KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS
+         G(KC_Z),    G(KC_X),    G(KC_C),    G(KC_V),     KC_PSCR,        KC_NO,    KC_F7,     KC_F8,     KC_F9,     KC_F11,
+         KC_SFT_UDO, KC_ALT_CUT, KC_GUI_CPY, KC_CTL_PST,  KC_VOLU,        KC_NO,    KC_CTL_F4, KC_GUI_F5, KC_ALT_F6, KC_SFT_F10,
+         KC_MUTE,    KC_MPRV,    KC_MPLY,    KC_MNXT,     KC_VOLD,        KC_NO,    KC_F1,     KC_F2,     KC_F3,     KC_F12,
+                                 QK_BOOT,    KC_TRNS,     KC_TRNS,        KC_TRNS,  KC_TRNS,   KC_TRNS
     ),
     // [_BLANK] = LAYOUT_split_3x5_3(
     //      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
