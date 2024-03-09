6304c1d54e0ff03feb4d7f63d5d82b7923251213
Dane Lipscombe <danelipscombe@gmail.com>
2024-03-09 16:36:25 +1100

    Setup layers

--- keyboards/mushi/keymaps/engram/keymap.c
+++ keyboards/mushi/keymaps/engram/keymap.c
@@ -15,7 +15,8 @@ enum custom_keycodes {
 
 enum mylayers {
     _BSE,
-    _NNM,
+    _SYN,
+    _NFN,
     _MED,
 };
 
@@ -30,63 +31,68 @@ enum mylayers {
 #define KC_ALT_S MT(MOD_LALT, KC_S)
 #define KC_SFT_N MT(MOD_LSFT, KC_N)
 
-#define KC_MED MO(_MED)
 
+#define KC_SFT_DEL MT(MOD_LSFT, KC_DEL)
+#define KC_ALT_SCN MT(MOD_LALT, KC_SCLN)
+#define KC_GUI_CMA MT(MOD_LGUI, KC_COMMA)
+#define KC_CTL_DOT MT(MOD_LCTL, KC_DOT)
+#define KC_CAG_MIN LCAG_T(KC_MINUS)
+#define KC_CAG_2 LCAG_T(KC_2)
+
+#define KC_CTL_4 MT(MOD_LCTL, KC_4)
+#define KC_GUI_5 MT(MOD_LGUI, KC_5)
+#define KC_ALT_6 MT(MOD_LALT, KC_6)
+#define KC_SFT_0 MT(MOD_LSFT, KC_0)
+
+
+#define KC_SFT_PDN MT(MOD_LALT, KC_PGDN)
 #define KC_ALT_LFT MT(MOD_LALT, KC_LEFT)
 #define KC_GUI_DWN MT(MOD_LGUI, KC_DOWN)
 #define KC_CTL_RGT MT(MOD_LCTL, KC_RIGHT)
 #define KC_SFT_END MT(MOD_LSFT, KC_END)
 #define KC_CAG_PGDN LCAG_T(KC_PGDN)
 
-#define KC_CAG_2 LCAG_T(KC_2)
-#define KC_CTL_4 MT(MOD_LCTL, KC_4)
-#define KC_GUI_5 MT(MOD_LGUI, KC_5)
-#define KC_ALT_6 MT(MOD_LALT, KC_6)
-#define KC_SFT_0 MT(MOD_LSFT, KC_0)
-
-#define KC_SFT_UDO MT(MOD_LSFT, C(KC_Z))
-#define KC_ALT_CUT MT(MOD_LALT, C(KC_X))
-#define KC_GUI_CPY MT(MOD_LGUI, C(KC_C))
-#define KC_CTL_PST MT(MOD_LCTL, C(KC_V))
+#define KC_CTL_F4 MT(MOD_LCTL, KC_F4)
+#define KC_GUI_F5 MT(MOD_LGUI, KC_F5)
+#define KC_ALT_F6 MT(MOD_LALT, KC_F6)
+#define KC_SFT_F10 MT(MOD_LSFT, KC_F10)
 
 #define KC_SFT_PSC MT(MOD_LALT, KC_PSCR)
 #define KC_ALT_PRV MT(MOD_LALT, KC_MPRV)
 #define KC_GUI_PLY MT(MOD_LGUI, KC_MPLY)
 #define KC_CTL_NXT MT(MOD_LCTL, KC_MNXT)
 
-#define KC_CTL_F4 MT(MOD_LCTL, KC_F4)
-#define KC_GUI_F5 MT(MOD_LGUI, KC_F5)
-#define KC_ALT_F6 MT(MOD_LALT, KC_F6)
-#define KC_SFT_F10 MT(MOD_LSFT, KC_F10)
-
-#define KC_SFT_BSPC MT(MOD_LSFT, KC_BSPC)
-#define KC_NNM_TAB LT(_NNM, KC_TAB)
-#define KC_MED_SPC LT(_MED, KC_SPC)
-
-#define KC_COMBO_SFT KC_SFT_BSPC
-#define KC_COMBO_ALT1 KC_NNM_TAB
-#define KC_COMBO_ALT2 KC_MED_SPC
+#define KC_MED MO(_MED)
+#define KC_COMBO_SFT MT(MOD_LSFT, KC_BSPC)
+#define KC_COMBO_ALT1 LT(_SYN, KC_TAB)
+#define KC_COMBO_ALT2 LT(_NFN, KC_SPC)
 
 #include "g/keymap_combo.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BSE] = LAYOUT_split_3x5_3(
-         KC_B,     KC_Y,     KC_O,     KC_U,       KC_NO,            KC_NO,       KC_L,       KC_D,        KC_W,     KC_V,
-         KC_SFT_C, KC_ALT_I, KC_GUI_E, KC_CTL_A,   KC_NO,            KC_NO,       KC_CTL_H,   KC_GUI_T,    KC_ALT_S, KC_SFT_N,
-         KC_G,     KC_X,     KC_CAG_J, KC_K,       KC_NO,            KC_NO,       KC_R,       KC_CAG_M,    KC_F,     KC_P,
-                             KC_MED,   KC_NNM_TAB, KC_MED_SPC,       KC_SFT_BSPC, KC_COMBO,   QK_REPEAT_KEY
+         KC_B,     KC_Y,     KC_O,     KC_U,          KC_NO,            KC_NO,        KC_L,       KC_D,        KC_W,     KC_V,
+         KC_SFT_C, KC_ALT_I, KC_GUI_E, KC_CTL_A,      KC_NO,            KC_NO,        KC_CTL_H,   KC_GUI_T,    KC_ALT_S, KC_SFT_N,
+         KC_G,     KC_X,     KC_CAG_J, KC_K,          KC_NO,            KC_NO,        KC_R,       KC_CAG_M,    KC_F,     KC_P,
+                             KC_NO,    KC_COMBO_ALT1, KC_COMBO_ALT2,    KC_COMBO_SFT, KC_COMBO,   KC_NO
     ),
-    [_NNM] = LAYOUT_split_3x5_3(
-         KC_GRV,      KC_ESC,     KC_UP,       KC_ENT,     KC_DEL,         KC_BSLS,  KC_7,     KC_8,     KC_9,     KC_SLSH,
-         KC_SFT_BSPC, KC_ALT_LFT, KC_GUI_DWN,  KC_CTL_RGT, KC_LBRC,        KC_EQL,   KC_CTL_4, KC_GUI_5, KC_ALT_6, KC_SFT_0,
-         KC_HOME,     KC_PGUP,    KC_CAG_PGDN, KC_SFT_END, KC_RBRC,        KC_MINUS, KC_1,     KC_CAG_2, KC_3,     KC_DOT,
-                                  KC_TRNS,     KC_TRNS,    KC_TRNS,        KC_TRNS,  KC_TRNS,  KC_TRNS
+    [_SYN] = LAYOUT_split_3x5_3(
+         KC_GRV,      KC_PSCR,    KC_ESC,     KC_ENT,     KC_NO,      KC_NO,   KC_7,     KC_8,     KC_9,     KC_BSLS,
+         KC_SFT_DEL,  KC_ALT_SCN, KC_GUI_CMA, KC_CTL_DOT, KC_NO,      KC_NO,   KC_CTL_4, KC_GUI_5, KC_ALT_6, KC_SFT_0,
+         KC_LBRC,     KC_RBRC,    KC_CAG_MIN, KC_EQL,     KC_NO,      KC_NO,   KC_1,     KC_CAG_2, KC_3,     KC_SLSH,
+                                  KC_TRNS,    KC_TRNS,    KC_MED,     KC_TRNS, KC_TRNS,  KC_TRNS
+    ),
+    [_NFN] = LAYOUT_split_3x5_3(
+         KC_PGUP,    KC_HOME,    KC_UP,      KC_END,     KC_NO,      KC_NO,   KC_F7,     KC_F8,     KC_F9,     KC_F11,
+         KC_SFT_PDN, KC_ALT_LFT, KC_GUI_DWN, KC_CTL_RGT, KC_NO,      KC_NO,   KC_CTL_F4, KC_GUI_F5, KC_ALT_F6, KC_SFT_F10,
+         KC_CUDO,    KC_CCUT,    KC_CCPY,    KC_CPST,    KC_NO,      KC_NO,   KC_F1,     KC_F2,     KC_F3,     KC_F12,
+                                 QK_BOOT,    KC_MED,     KC_TRNS,    KC_TRNS, KC_TRNS,   KC_TRNS
     ),
     [_MED] = LAYOUT_split_3x5_3(
-         KC_CUDO,    KC_CCUT,    KC_CCPY,    KC_CPST,     KC_BTN1,        KC_NO,    KC_F7,     KC_F8,     KC_F9,     KC_F11,
-         KC_SFT_PSC, KC_ALT_PRV, KC_GUI_PLY, KC_CTL_NXT,  KC_VOLU,        KC_NO,    KC_CTL_F4, KC_GUI_F5, KC_ALT_F6, KC_SFT_F10,
-         C(KC_Z),    C(KC_X),    C(KC_C),    C(KC_V),     KC_VOLD,        KC_NO,    KC_F1,     KC_F2,     KC_F3,     KC_F12,
-                                 QK_BOOT,    KC_TRNS,     KC_TRNS,        KC_TRNS,  KC_TRNS,   KC_TRNS
+         KC_NO,      KC_NO,      KC_VOLD,    KC_VOLU,     KC_NO,      KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,
+         KC_SFT_PSC, KC_ALT_PRV, KC_GUI_PLY, KC_CTL_NXT,  KC_NO,      KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,
+         KC_NO,      KC_NO,      KC_NO,      KC_NO,       KC_NO,      KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,
+                                 QK_BOOT,    KC_TRNS,     KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS
     ),
     // [_BLANK] = LAYOUT_split_3x5_3(
     //      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
@@ -98,9 +104,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case KC_SFT_BSPC:
+        case KC_COMBO_SFT:
             return 0;
-        case KC_MED_SPC:
+        case KC_COMBO_ALT2:
             return 0;
         default:
             return QUICK_TAP_TERM;
@@ -109,8 +115,8 @@ uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case KC_SFT_BSPC:
-        case KC_NNM_TAB:
+        case KC_COMBO_SFT:
+        case KC_COMBO_ALT1:
             return true;
         default:
             return false;
