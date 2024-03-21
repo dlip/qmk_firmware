ff02daab6b5f206803968f0a7b079c21a77eaa09
Dane Lipscombe <danelipscombe@gmail.com>
2024-03-21 18:35:59 +1100

    Update tamatama

--- keyboards/tamatama/keymaps/default/keymap.c
+++ keyboards/tamatama/keymaps/default/keymap.c
@@ -5,9 +5,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x4_2(
-        KC_Q,    KC_W,    KC_E,    KC_R,      KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,      KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,      KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                          KC_1,    KC_2,      KC_3,    KC_4,
+        KC_A,    KC_B,    KC_C,    KC_D,      KC_M,    KC_N,    KC_O,    KC_P,
+        KC_E,    KC_F,    KC_G,    KC_H,      KC_Q,    KC_R,    KC_S,    KC_T,
+        KC_I,    KC_J,    KC_K,    KC_L,      KC_U,    KC_V,    KC_W,    KC_X,
+        KC_NO,   KC_NO,   KC_1,    KC_2,      KC_3,    KC_4,    KC_NO,   KC_NO
     )
 };
