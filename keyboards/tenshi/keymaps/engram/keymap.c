237ce57f2167bd83e04ac1efc6c6ae2ea3b4228b
Dane Lipscombe <danelipscombe@gmail.com>
2024-07-28 11:25:40 +1000

    Add gaming nav

--- keyboards/tenshi/keymaps/engram/keymap.c
+++ keyboards/tenshi/keymaps/engram/keymap.c
@@ -107,16 +107,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  KC_TRNS,     KC_TRNS,       KC_TRNS,   KC_TRNS
     ),
     [_GA1] = LAYOUT_split_3x4_4(
-         KC_TAB,  KC_Q,  KC_W,    KC_E,      KC_NO, KC_NO, KC_NO, KC_NO,
-         KC_LSFT, KC_A,  KC_S,    KC_D,      KC_NO, KC_NO, KC_NO, KC_NO,
-         KC_LCTL, KC_Z,  KC_X,    KC_C,      KC_NO, KC_NO, KC_NO, KC_NO,
-                         KC_BTN2, KC_BTN1,   KC_NO, KC_NO,
-                         KC_GA2,  KC_SPC,    KC_NO, KC_NO
+         KC_TAB,  KC_Q,  KC_W,    KC_E,      KC_ESC,  KC_UP,   KC_ENTER, KC_NO,
+         KC_LSFT, KC_A,  KC_S,    KC_D,      KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO,
+         KC_LCTL, KC_Z,  KC_X,    KC_C,      KC_NO,   KC_NO,   KC_NO, KC_NO,
+                         KC_BTN2, KC_BTN1,   KC_NO,   KC_NO,
+                         KC_GA2,  KC_SPC,    KC_BTN2, KC_BSPC
     ),
     [_GA2] = LAYOUT_split_3x4_4(
-         KC_NO,  KC_7, KC_8,  KC_9,     KC_NO, KC_NO, KC_NO, KC_NO,
-         KC_NO,  KC_4, KC_5,  KC_6,     KC_NO, KC_NO, KC_NO, KC_NO,
-         KC_GA1, KC_1, KC_2,  KC_3,     KC_NO, KC_NO, KC_NO, KC_NO,
+         KC_4,   KC_5, KC_6,  KC_R,     KC_F7, KC_F8, KC_F9, KC_F11,
+         KC_1,   KC_2, KC_3,  KC_F,     KC_F4, KC_F5, KC_F6, KC_F10,
+         KC_GA1, KC_7, KC_8,  KC_V,     KC_F1, KC_F2, KC_F3, KC_F12,
                        KC_NO, KC_NO,    KC_NO, KC_NO,
                        KC_NO, KC_NO,    KC_NO, KC_NO
     )
