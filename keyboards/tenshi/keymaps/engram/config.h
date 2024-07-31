8618e8ebc61f8d491dceb03555d92a01f7e7aae9
Dane Lipscombe <danelipscombe@gmail.com>
2024-07-31 19:18:46 +1000

    Change timeout

--- keyboards/tenshi/keymaps/engram/config.h
+++ keyboards/tenshi/keymaps/engram/config.h
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
-#define ONESHOT_TIMEOUT 500
+#define ONESHOT_TIMEOUT 1000
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 #define QUICK_TAP_TERM_PER_KEY
 #define COMBO_VARIABLE_LEN
