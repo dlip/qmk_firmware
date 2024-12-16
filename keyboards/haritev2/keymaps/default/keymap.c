3da8740c81c42fef5f678b8ae729f3ca86e702cb
Dane Lipscombe <danelipscombe@gmail.com>
2024-12-16 16:40:13 +1100

    add dip

--- keyboards/haritev2/keymaps/default/keymap.c
+++ keyboards/haritev2/keymaps/default/keymap.c
@@ -26,11 +26,28 @@ void keyboard_post_init_user(void) {
   debug_mouse=true;
 }
 
+#if defined(DIP_SWITCH_MAP_ENABLE)
+const uint16_t PROGMEM dip_switch_map[NUM_DIP_SWITCHES][NUM_DIP_STATES] = {
+    DIP_SWITCH_OFF_ON(DF(0), DF(1)),
+};
+#endif
+
+bool dip_switch_update_user(uint8_t index, bool active) {
+    print("dip");
+    switch (index) {
+        case 0:
+            if(active) { tap_code(KC_A); } else {  }
+            break;
+    }
+    return true;
+}
+
 #ifdef JOYSTICK_ENABLE
     /* Joystick axes settings */
-    joystick_config_t joystick_axes[JOYSTICK_AXIS_COUNT] = {
-        [0] = JOYSTICK_AXIS_IN(GP26, 900, 575, 285),
-        [1] = JOYSTICK_AXIS_IN(GP27, 900, 575, 285)
-    };
+    // joystick_config_t joystick_axes[JOYSTICK_AXIS_COUNT] = {
+    //     [0] = JOYSTICK_AXIS_IN(GP26, 900, 575, 285),
+    //     [1] = JOYSTICK_AXIS_IN(GP27, 900, 575, 285)
+    // };
 #endif
+
 
