e8ac57a4d0273010f1cf68d2c7b9127c0b35bc68
Dane Lipscombe <danelipscombe@gmail.com>
2024-12-17 17:31:57 +1100

    scrolling wip

--- keyboards/haritev2/keymaps/default/keymap.c
+++ keyboards/haritev2/keymaps/default/keymap.c
@@ -1,6 +1,7 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include "print.h"
+#include "timer.h"
 
 #include QMK_KEYBOARD_H
 #ifdef JOYSTICK_ENABLE
@@ -24,12 +25,175 @@ void keyboard_post_init_user(void) {
   debug_matrix=false;
   debug_keyboard=true;
   debug_mouse=true;
+
 }
 
 #ifdef POINTING_DEVICE_ENABLE
 // Modify these values to adjust the scrolling speed
-#define SCROLL_DIVISOR_H 5.0
-#define SCROLL_DIVISOR_V 5.0
+#define SCROLL_DIVISOR_H 3.0
+#define SCROLL_DIVISOR_V 3.0
+#define DEADZONE 1
+#define SCROLL_MAX 2A
+int16_t scrollMinAxisValues[2] = {0,0};
+int16_t scrollMaxAxisValues[2] = {0,0};
+#define MAX_SPEED 2.f
+#define MAX_RATE 100
+#define MIN_RATE 500
+
+uint16_t scroll_timer_x = 0;
+uint16_t scroll_timer_y = 0;
+// auto range with time
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+    if (mouse_report.x < scrollMinAxisValues[0]) {
+        scrollMinAxisValues[0] = mouse_report.x;
+    }
+    if (mouse_report.x > scrollMaxAxisValues[0]) {
+        scrollMaxAxisValues[0] = mouse_report.x;
+    }
+    if (mouse_report.y < scrollMinAxisValues[1]) {
+        scrollMinAxisValues[1] = mouse_report.y;
+    }
+    if (mouse_report.y > scrollMaxAxisValues[1]) {
+        scrollMaxAxisValues[1] = mouse_report.y;
+    }
+
+    // uprintf("x: %i\n", mouse_report.x);
+    // uprintf("y: %i\n", mouse_report.y);
+
+    const uint16_t time_elapsed_x = timer_elapsed(scroll_timer_x);
+    const uint16_t time_elapsed_y = timer_elapsed(scroll_timer_y);
+    if (mouse_report.x < -DEADZONE) {
+        float percent = (float)mouse_report.x / (float)scrollMinAxisValues[0];
+        float rate = MIN_RATE - (percent * (MIN_RATE - MAX_RATE));
+        uprintf("time: %i\n", time_elapsed_x);
+        uprintf("rate: %i\n", (int16_t)rate);
+        if (time_elapsed_x > rate) {
+            mouse_report.h = -1;
+            scroll_timer_x = timer_read();
+        }
+    }
+    else if (mouse_report.x > DEADZONE) {
+        float percent = (float)mouse_report.x / (float)scrollMaxAxisValues[0];
+        float rate = MIN_RATE - (percent * (MIN_RATE - MAX_RATE));
+        if (time_elapsed_x > rate) {
+            mouse_report.h = 1;
+            scroll_timer_x = timer_read();
+        }
+    }
+    if (mouse_report.y < -DEADZONE) {
+        float percent = (float)mouse_report.y / (float)scrollMinAxisValues[1];
+        float rate = MIN_RATE - (percent * (MIN_RATE - MAX_RATE));
+        if (time_elapsed_y > rate) {
+            mouse_report.v = -1;
+            scroll_timer_y = timer_read();
+        }
+    }
+    else if (mouse_report.y > 0) {
+        float percent = (float)mouse_report.y / (float)scrollMaxAxisValues[1];
+        float rate = MIN_RATE - (percent * (MIN_RATE - MAX_RATE));
+        if (time_elapsed_y > rate) {
+            mouse_report.v = 1;
+            scroll_timer_y = timer_read();
+        }
+    }
+    uprintf("sx: %i\n", mouse_report.x);
+    uprintf("sy: %i\n", mouse_report.y);
+    mouse_report.x = 0;
+    mouse_report.y = 0;
+    return mouse_report;
+}
+// basic clamp
+// report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+//     if (mouse_report.x > DEADZONE) {
+//         mouse_report.h = mouse_report.x/mouse_report.x;
+//     }
+//     if (mouse_report.x < -DEADZONE) {
+//         mouse_report.h = -(mouse_report.x/mouse_report.x);
+//     }
+//     if (mouse_report.y > DEADZONE) {
+//         mouse_report.v = -(mouse_report.y/mouse_report.y);
+//     }
+//     if (mouse_report.y < -DEADZONE) {
+//         mouse_report.v = mouse_report.y/mouse_report.y;
+//     }
+//
+//     uprintf("x: %i\n", mouse_report.x);
+//     uprintf("y: %i\n", mouse_report.y);
+//     uprintf("v: %i\n", mouse_report.v);
+//     uprintf("h: %i\n", mouse_report.h);
+//     mouse_report.x = 0;
+//     mouse_report.y = 0;
+//     return mouse_report;
+// }
+// auto range
+// report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+//     if (mouse_report.x < scrollMinAxisValues[0]) {
+//         scrollMinAxisValues[0] = mouse_report.x;
+//     }
+//     if (mouse_report.x > scrollMaxAxisValues[0]) {
+//         scrollMaxAxisValues[0] = mouse_report.x;
+//     }
+//     if (mouse_report.y < scrollMinAxisValues[1]) {
+//         scrollMinAxisValues[1] = mouse_report.y;
+//     }
+//     if (mouse_report.y > scrollMaxAxisValues[1]) {
+//         scrollMaxAxisValues[1] = mouse_report.y;
+//     }
+//     uprintf("x: %i\n", mouse_report.x);
+//     uprintf("y: %i\n", mouse_report.y);
+//     if (mouse_report.x < 0) {
+//         float percent = (float)mouse_report.x / (float)scrollMinAxisValues[0];
+//         mouse_report.x = -(int8_t)(percent * MAX_SPEED);
+//     }
+//     else if (mouse_report.x > 0) {
+//         float percent = (float)mouse_report.x / (float)scrollMaxAxisValues[0];
+//         mouse_report.x = (int8_t)(percent * MAX_SPEED);
+//     }
+//     if (mouse_report.y < 0) {
+//         float percent = (float)mouse_report.y / (float)scrollMinAxisValues[1];
+//         mouse_report.y = -(int8_t)(percent * MAX_SPEED);
+//     }
+//     else if (mouse_report.y > 0) {
+//         float percent = (float)mouse_report.y / (float)scrollMaxAxisValues[1];
+//         mouse_report.y = (int8_t)(percent * MAX_SPEED);
+//     }
+//
+//     uprintf("sx: %i\n", mouse_report.x);
+//     uprintf("sy: %i\n", mouse_report.y);
+//     mouse_report.h = mouse_report.x;
+//     mouse_report.v = mouse_report.y;
+//     mouse_report.x = 0;
+//     mouse_report.y = 0;
+//     return mouse_report;
+// }
+// report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+//     // return mouse_report;
+//     if (mouse_report.x < 0) {
+//         mouse_report.x *= 2;
+//     }
+//     if (mouse_report.y < 0) {
+//         mouse_report.y *= 2;
+//     }
+//     if (abs(mouse_report.x) <= DEADZONE) {
+//         mouse_report.x = 0;
+//     }
+//     if (abs(mouse_report.y) <= DEADZONE) {
+//         mouse_report.y = 0;
+//     }
+//     if (abs(mouse_report.x) >= SCROLL_MAX) {
+//         mouse_report.x = SCROLL_MAX;
+//     }
+//     if (abs(mouse_report.y) >= SCROLL_MAX) {
+//         mouse_report.y = SCROLL_MAX;
+//     }
+//     uprintf("x: %i\n", mouse_report.x);
+//     uprintf("y: %i\n", mouse_report.y);
+//     mouse_report.h = mouse_report.x;
+//     mouse_report.v = -mouse_report.y;
+//     mouse_report.x = 0;
+//     mouse_report.y = 0;
+//     return mouse_report;
+// }
 // report_mouse_t pointing_device_task_combined_user(report_mouse_t left_report, report_mouse_t right_report) {
 //     // left_report.h = (int8_t)((float)left_report.x / SCROLL_DIVISOR_H);
 //     // left_report.v = (int8_t)((float)-left_report.y / SCROLL_DIVISOR_V);
@@ -67,8 +231,8 @@ bool dip_switch_update_user(uint8_t index, bool active) {
 
 #ifdef JOYSTICK_ENABLE
     joystick_config_t joystick_axes[JOYSTICK_AXIS_COUNT] = {
-    [0] = JOYSTICK_AXIS_IN(GP26, 25, 312, 807),
-    [1] = JOYSTICK_AXIS_IN(GP27, 37, 402, 822),
+    [0] = JOYSTICK_AXIS_IN(GP26, ANALOG_JOYSTICK_X_AXIS_MIN, ANALOG_JOYSTICK_X_AXIS_ORIGIN, ANALOG_JOYSTICK_X_AXIS_MAX),
+    [1] = JOYSTICK_AXIS_IN(GP27, ANALOG_JOYSTICK_Y_AXIS_MIN, ANALOG_JOYSTICK_Y_AXIS_ORIGIN, ANALOG_JOYSTICK_Y_AXIS_MAX),
     };
 #endif
 
