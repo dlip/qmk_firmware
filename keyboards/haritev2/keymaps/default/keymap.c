47b40fefa341357c0036287484c34f75490e57e9
Dane Lipscombe <danelipscombe@gmail.com>
2025-01-01 14:10:14 +1100

    haritev2 matrix

--- keyboards/haritev2/keymaps/default/keymap.c
+++ keyboards/haritev2/keymaps/default/keymap.c
@@ -9,10 +9,14 @@
 #endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_split_5x5(
+    [0] = LAYOUT_split_5x6(
               KC_U,              KC_U,              KC_U,              KC_U,                    KC_U,              KC_U,              KC_U,              KC_U,
         KC_L, KC_C, KC_R,  KC_L, KC_C, KC_R,  KC_L, KC_C, KC_R,  KC_L, KC_C, KC_R,        KC_L, KC_C, KC_R,  KC_L, KC_C, KC_R,  KC_L, KC_C, KC_R,  KC_L, KC_C, KC_R,
               KC_D,              KC_D,              KC_D,              KC_D,                    KC_D,              KC_D,              KC_D,              KC_D,
+
+                                                                       KC_U,                    KC_U,
+                                                                 KC_L, KC_C, KC_R,        KC_L, KC_C, KC_R,
+                                                                       KC_D,                    KC_D,
 
                                                                        KC_U,                    KC_U,
                                                                  KC_L, KC_C, KC_R,        KC_L, KC_C, KC_R,
