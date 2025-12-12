2d1a98ca76cba094dd16392be62f00d8cd88dccb
Dane Lipscombe <danelipscombe@gmail.com>
2025-12-12 16:11:06 +1100

    Mouse layer

--- keyboards/dlip/tenshi/keymaps/engram/keymap.c
+++ keyboards/dlip/tenshi/keymaps/engram/keymap.c
@@ -26,6 +26,7 @@ enum mylayers {
     _FUN,
     _GA1,
     _GA2,
+    _MSE,
 };
 
 #define KC_SFT_C MT(MOD_LSFT, KC_C)
@@ -122,7 +123,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_GA1, KC_7, KC_8,  KC_G,     KC_F1, KC_F2, KC_F3, KC_F12,
                        KC_NO, KC_NO,    KC_NO, KC_NO,
                        KC_NO, KC_NO,    KC_NO, KC_NO
-    )
+    ),
+    [_MSE] = LAYOUT_split_3x4_4(
+         KC_TRNS,    MS_BTN3,    MS_BTN2,    MS_BTN1,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+         KC_SFT_BSP, KC_ALT_DEL, KC_GUI_ESC, KC_CTL_ENT,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+         KC_CUDO,    KC_CCUT,    KC_CAG_CPY, KC_CPST,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                 KC_TRNS,    KC_TRNS,     KC_TRNS, KC_TRNS,
+                                 KC_TRNS,    KC_TRNS,     KC_TRNS, KC_TRNS
+    ),
     // [_BLANK] = LAYOUT_split_3x4_4(
     //      KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
     //      KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
@@ -140,6 +148,11 @@ void keyboard_post_init_user(void) {
     // pointing_device_set_cpi_on_side(false, 300);
     // pointing_device_set_cpi_on_side(true, PMW33XX_CPI * 2);
 #endif
+}
+
+void pointing_device_init_user(void) {
+    set_auto_mouse_layer(_MSE); // only required if AUTO_MOUSE_DEFAULT_LAYER is not set to index of <mouse_layer>
+    set_auto_mouse_enable(true);         // always required before the auto mouse feature will work
 }
 
 // Modify these values to adjust the scrolling speed
