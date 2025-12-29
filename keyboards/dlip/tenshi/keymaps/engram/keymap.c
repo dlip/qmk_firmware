5521cd9c768765365116c1adcc3b85fe18139b95
Dane Lipscombe <danelipscombe@gmail.com>
2025-12-29 13:50:00 +1100

    media

--- keyboards/dlip/tenshi/keymaps/engram/keymap.c
+++ keyboards/dlip/tenshi/keymaps/engram/keymap.c
@@ -62,7 +62,7 @@ enum mylayers {
 #define KC_ALT_RGT MT(MOD_LALT, KC_RIGHT)
 #define KC_SFT_PGD MT(MOD_LSFT, KC_PGDN)
 
-#define KC_SFT_MUT MT(MOD_LSFT, KC_MUTE)
+#define KC_SFT_STP MT(MOD_LSFT, KC_MSTP)
 #define KC_ALT_PRV MT(MOD_LALT, KC_MPRV)
 #define KC_GUI_PLY MT(MOD_LGUI, KC_MPLY)
 #define KC_CTL_NXT MT(MOD_LCTL, KC_MNXT)
@@ -104,8 +104,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    KC_FUN,     KC_TRNS,       KC_TRNS,    KC_TRNS
     ),
     [_FUN] = LAYOUT_split_3x4_4(
-         QK_BOOT,    KC_NO,      KC_VOLD,     KC_VOLU,       KC_F7,     KC_F8,     KC_F9,     KC_F11,
-         KC_SFT_MUT, KC_ALT_PRV, KC_GUI_PLY,  KC_CTL_NXT,    KC_CTL_F4, KC_GUI_F5, KC_ALT_F6, KC_SFT_F10,
+         QK_BOOT,    KC_VOLD,    KC_MUTE,     KC_VOLU,       KC_F7,     KC_F8,     KC_F9,     KC_F11,
+         KC_SFT_STP, KC_ALT_PRV, KC_GUI_PLY,  KC_CTL_NXT,    KC_CTL_F4, KC_GUI_F5, KC_ALT_F6, KC_SFT_F10,
          KC_GA1,     KC_NO,      KC_CAG_BRD,  KC_BRIU,       KC_F1,     KC_CAG_F2, KC_F3,     KC_F12,
                                  KC_TRNS,     KC_TRNS,       KC_TRNS,   KC_TRNS,
                                  KC_TRNS,     KC_TRNS,       KC_TRNS,   KC_TRNS
